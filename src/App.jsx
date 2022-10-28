9951ac01cfe950c2658ebf700af243c71bbc5511
ENG-SKYLER <ibraheema.a2014@gmail.com>
2022-10-28 19:56:32 +0200

    Quizzical Starter Page Structure and Css modification

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import './App.css'
+import Start from "./Components/Start"
+
+function App() {
+return (
+
+  <Start />
+    )
+
+}
+
+export default App
---
+++ src/Components/Start.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import pattern1  from '../Assets/blue-pattern.svg'
+import pattern2  from '../Assets/yellow-pattern.svg'
+
+
+function Start() {
+  return (
+    <div className='start'>
+        <h1>Quizzical</h1>
+        <p>Some description if needed</p>
+        <button> Start quiz</button>
+
+<div className="pattern1">
+    <img src= {pattern1} alt="" />
+</div>
+<div className="pattern2">
+<img src= {pattern2} alt="" />
+</div>
+    </div>
+  )
+}
+
+export default Start
