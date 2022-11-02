51d5197454baed36f1f33d0e6328f47d08d84484
ENG-SKYLER <ibraheema.a2014@gmail.com>
2022-11-02 22:13:27 +0200

    update component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import Form from "./Components/Form"
 import Start from "./Components/Start"
 
 function App() {
+  const [questions , setQuestions] = React.useState([])
+
+  React.useEffect(()=> {
+    fetch('https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple').then(res => res.json()).then(data => setQuestions(data))
+
+  },[])
+
+  console.log(questions)
+
 return (
 
   // <Start />
