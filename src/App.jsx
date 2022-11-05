72fcc844da54e28f232cca9a033ece823f69e236
ENG-SKYLER <ibraheema.a2014@gmail.com>
2022-11-05 21:08:21 +0200

    API modification

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,14 @@ import axios from "axios"
 
 function App() {
   const [questions , setQuestions] = React.useState([])
-  const [correctAnswer , setCorrectAnswer] = React.useState([{
-    isCorrect : true
-  }]);
-  const [remainingAnswer , setRemainingAnswer] = React.useState([{
-  }])
-  let correct = [];
-  let answer = [];
+  // const [answers , setAnswers] = React.useState([])
+  // const [correct , setCorrect] = React.useState([])
 
-  const quiz = questions.results && questions.results.map(data => {
-    correct.push(data.correct_answer)
-    answer.push([...data.incorrect_answers])
-    return   <Form quiz={data.question} ans={data.incorrect_answers} />
-  })
+  // const quiz = questions.results && questions.results.map(data => {
+  //   correct.push(data.correct_answer)
+  //   answer.push([...data.incorrect_answers])
+  //   return   <Form quiz={data.question} ans={data.incorrect_answers} />
+  // })
 
   // React.useEffect(()=>{
   //   setCorrectAnswer(prevValue => {
@@ -61,24 +56,49 @@ function App() {
     const fetchData =  async () => {
       const data = await fetch('https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple');
       const json = await data.json();
-      dataReallocated(json.results)
+      setQuestions(json.results.map( result => {
+        return ({
+          question : result.question,
+          correct : result.correct_answer,
+          answers : result.incorrect_answers.concat([result.correct_answer]).map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value),
+          // answers : result.incorrect_answers.splice(Math.floor(Math.random()) * (result.incorrect_answers.length + 1),0 ,result.correct_answerr )
+          ChoosedAnswer : undefined,
+        })
+      }))
     }
     fetchData()
   },[])
-
-  const dataReallocated = useCallback((data) => {
-    data && data.map((answers) => {
-      let correctAnswers = answers.correct_answer;
-      let incorrectAnswers = answers.incorrect_answers;
-      let allAnswers = incorrectAnswers
-      allAnswers.splice(Math.floor(Math.random() * (incorrectAnswers.length + 1)) ,0 ,correctAnswers )
-      console.log(allAnswers)
-      console.log(correctAnswers)
-    })
-  }, [])
+  const quiz = questions.map((quizz, index) => {
+    return <Form 
+    quiz={quizz.question} 
+    ans={quizz.answers}
+    id = {index}
+    key = {index} />
+  })
 
 
+  // const dataReallocated = useCallback((data) => {
+  //   // data && data.map((answers) => {
+  //   //   // let correctAnswers = answers.correct_answer;
+  //   //   // setCorrect(correctAnswers)
+  //   //   // let incorrectAnswers = answers.incorrect_answers;
+  //   //   // let  allAnswers = incorrectAnswers
+  //   //   // allAnswers.splice(Math.floor(Math.random() * (incorrectAnswers.length + 1)) ,0 ,correctAnswers )
+  //   // })
+  //   setQuestions(data)
+  // }, [])
 
+// let correctAnswers ;
+//   const quiz = questions && questions.map((quizz) => {
+//      correctAnswers = quizz.correct_answer;
+//     let  incorrectAnswers = quizz.incorrect_answers;
+//     let  allAnswers = incorrectAnswers
+//     allAnswers.splice(Math.floor(Math.random() * (incorrectAnswers.length + 1)) ,0 ,correctAnswers )
+//     console.log(correctAnswers)
+//     return <Form quiz={quizz.question} ans={allAnswers} />
+//   })
+
+  // setCorrect(correctAnswers)
 
   
   // const form = questions.map((data) => { 
